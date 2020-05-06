02cc38311e031bee7672f3105dd6de53fbb74da7
Fernando González Cortés <fernando@parknav.com>
2020-05-06 08:46:53 +0200

    configuration 3

--- src/main/java/com/parknav/readylive/ReadyliveController.java
+++ src/main/java/com/parknav/readylive/ReadyliveController.java
@@ -1,6 +1,10 @@
 package com.parknav.readylive;
 
 import com.github.kkuegler.PermutationBasedHumanReadableIdGenerator;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,12 @@ public class ReadyliveController {
     private boolean temporarilyBroken = false;
     private boolean broken = false;
 
+    private final ApplicationEventPublisher eventPublisher;
+
+    public ReadyliveController(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
     @PostConstruct
     public void initId() {
         this.id = new PermutationBasedHumanReadableIdGenerator().generate();
@@ -23,8 +33,12 @@ public class ReadyliveController {
     @GetMapping("id")
     public String hello() {
         if (temporarilyBroken) {
+            // We don't want to be bothered in the next 2 seconds.
+            AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.REFUSING_TRAFFIC);
             return "Service temporarily disabled";
         } else if (broken) {
+            // We realize something broke and we cannot recover from it. We want Kubernetes to restart the container.
+            AvailabilityChangeEvent.publish(eventPublisher, this, LivenessState.BROKEN);
             throw new RuntimeException("Service is broken");
         } else {
             return id;
@@ -39,6 +53,7 @@ public class ReadyliveController {
             public void run() {
                 // Back to normal
                 temporarilyBroken = false;
+                AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.ACCEPTING_TRAFFIC);
             }
         }, 4000L);
         return "Disabled 4s";
